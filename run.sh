42a54c6890b409d0fbd0b2498211c7a9b86a2a9c
Oliver Cheng <olliecheng@outlook.com>
2023-02-15 14:52:46 +0800

    Added zip of final build

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 pandoc sample.docx -f docx -t html -o index.html -s --metadata title="ACL" --css=style.css --filter ./filter.py --template template.html
+
+zip dist.zip template.html style.css index.html
