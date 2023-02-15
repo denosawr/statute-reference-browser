72d3b18dd1e5e6b1640ba7ea5c2c772342828dd6
Oliver Cheng <olliecheng@outlook.com>
2023-02-15 15:55:36 +0800

    Wrong file was uploaded to zip

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 pandoc "$1" -f docx -t html -o index.html -s --metadata title="ACL" --css=style.css --filter ./filter.py --template template.html
 
-zip dist.zip template.html style.css script.js
+zip dist.zip index.html style.css script.js
