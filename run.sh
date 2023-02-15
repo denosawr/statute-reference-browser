fe62a4c9ab0913d013cae1c473a2feb0d6465707
Oliver Cheng <olliecheng@outlook.com>
2023-02-15 15:41:59 +0800

    Added file parameter to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-pandoc sample.docx -f docx -t html -o index.html -s --metadata title="ACL" --css=style.css --filter ./filter.py --template template.html
+pandoc "$1" -f docx -t html -o index.html -s --metadata title="ACL" --css=style.css --filter ./filter.py --template template.html
 
-zip dist.zip template.html style.css index.html
+zip dist.zip template.html style.css script.js
